3a1de8166a8b9f86538d10b2761f173c23117c3f
swilliamsilva <simuleagora@gmail.com>
2025-05-16 11:12:32 -0300

    Atualizado - erro 502, identificado que aplicação tinha .htaccss duplicado. removido /system/.htaccess

--- build-release.sh
+++ build-release.sh
@@ -1,84 +1,37 @@
-#!/usr/bin/env bash
+# Ativa o mod_rewrite para redirecionamentos
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteBase /
+    
+    # Redireciona tudo para o index.php (para frameworks como CodeIgniter/Laravel)
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteRule ^(.*)$ index.php?/$1 [L,QSA]
+</IfModule>
 
-cd $(dirname $BASH_SOURCE)
+# Bloqueia acesso a arquivos/diretórios sensíveis
+<FilesMatch "\.(env|log|sql|ini|bak|conf|key)$">
+    <IfModule authz_core_module>
+        Require all denied
+    </IfModule>
+    <IfModule !authz_core_module>
+        Deny from all
+    </IfModule>
+</FilesMatch>
 
-if [ $# -eq 0 ]; then
-	echo 'Usage: '$BASH_SOURCE' <version_number>'
-	exit 1
-fi
+# Bloqueia acesso ao diretório /system (se existir)
+<Directory "/system">
+    <IfModule authz_core_module>
+        Require all denied
+    </IfModule>
+    <IfModule !authz_core_module>
+        Deny from all
+    </IfModule>
+</Directory>
 
-version_number=$1
+# Impede listagem de diretórios
+Options -Indexes
 
-if [ ${#version_number} -lt 5 ]
-then
-	echo "Provided version number is too short"
-	exit 1
-elif [ ${version_number: -4} == "-dev" ]
-then
-	echo "'-dev' releases are not allowed"
-	exit 1
-fi
-
-version_id=${version_number:0:5}
-version_id=${version_id//./}
-upgrade_rst='user_guide_src/source/installation/upgrade_'$version_id'.rst'
-
-if [ ${#version_id} -ne 3 ]
-then
-	echo "Invalid version number format"
-	exit 1
-elif [ `grep -c -F --regexp="'$version_number'" system/core/CodeIgniter.php` -ne 1 ]
-then
-	echo "Provided version number doesn't match in system/core/CodeIgniter.php"
-	exit 1
-elif [ `grep -c -F --regexp="'$version_number'" user_guide_src/source/conf.py` -ne 2 ]
-then
-	echo "Provided version number doesn't match in user_guide_src/source/conf.py"
-	exit 1
-elif [ `grep -c -F --regexp="$version_number (Current version) <https://codeload.github.com/bcit-ci/CodeIgniter/zip/$version_number>" user_guide_src/source/installation/downloads.rst` -ne 1 ]
-then
-	echo "user_guide_src/source/installation/downloads.rst doesn't appear to contain a link for this version"
-	exit 1
-elif [ ! -f "$upgrade_rst" ]
-then
-	echo "${upgrade_rst} doesn't exist"
-	exit 1
-fi
-
-echo "Running tests ..."
-
-php -d zend.enable_gc=0 -d date.timezone=UTC -d mbstring.func_overload=7 -d mbstring.internal_encoding=UTF-8 vendor/bin/phpunit --coverage-text --configuration tests/travis/sqlite.phpunit.xml
-
-if [ $? -ne 0 ]
-then
-	echo "Build FAILED!"
-	exit 1
-fi
-
-cd user_guide_src/
-
-echo ""
-echo "Building HTML docs; please check output for warnings ..."
-echo ""
-
-make html
-
-echo ""
-
-if [ $? -ne 0 ]
-then
-	echo "Build FAILED!"
-	exit 1
-fi
-
-cd ..
-
-if [ -d user_guide/ ]
-then
-	rm -r user_guide/
-fi
-
-cp -r user_guide_src/build/html/ user_guide/
-git add user_guide/
-
-echo "Build complete."
+# Redirecionamentos de erro
+ErrorDocument 403 "Acesso negado"
+ErrorDocument 404 "Página não encontrada"
