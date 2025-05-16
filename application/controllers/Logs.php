e05a446128091abf2b713d007a1e45c5993fda14
swilliamsilva <simuleagora@gmail.com>
2025-05-16 13:08:33 -0300

    Atualizado

---
+++ application/controllers/Logs.php
@@ -0,0 +1,9 @@
+// application/controllers/Logs.php
+class Logs extends CI_Controller {
+    public function index() {
+        header('Content-Type: text/plain');
+        echo file_get_contents('/var/www/html/application/logs/php_errors.log');
+        echo "\n\n";
+        echo file_get_contents('/var/log/apache2/error.log');
+    }
+}
