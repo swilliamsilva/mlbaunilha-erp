3232531173086765ca24d8fa2bca48a70df9b2d4
swilliamsilva <simuleagora@gmail.com>
2025-05-23 21:33:04 -0300

    Atualizado

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
-# Substitui dinamicamente a porta do Apache pela variável $PORT se estiver definida
+echo "✨ docker-entrypoint.sh: Iniciado! Porta = ${PORT}"
+
 if [[ -n "${PORT}" ]]; then
   sed -i "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf
   sed -i "s/<VirtualHost \*:80>/<VirtualHost \*:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+else
+  echo "⚠️ Variável PORT não definida!"
 fi
 
 exec "$@"
